adbbf6dccbc3a60be43298cc1f1aa4100216b086
sagunrajshahi1 <sagunrajshahi@gmail.com>
2026-07-06 17:10:48 -0600

    Support Render deployment

--- config/google.js
+++ config/google.js
@@ -1,16 +1,37 @@
 const { google } = require("googleapis");
-const path = require("path");
+
+let credentials;
+
+// Running on Render
+if (process.env.GOOGLE_SERVICE_ACCOUNT) {
+
+    credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
+
+} else {
+
+    // Running locally
+    credentials = require("../service-account.json");
+
+}
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, "..", "service-account.json"),
+
+    credentials,
+
     scopes: [
+
         "https://www.googleapis.com/auth/spreadsheets"
+
     ]
+
 });
 
 const sheets = google.sheets({
+
     version: "v4",
+
     auth
+
 });
 
 module.exports = sheets;
