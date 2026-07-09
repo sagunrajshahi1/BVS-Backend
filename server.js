e084a3ca2d39d4a1496e9e778ae94b066e2b6d20
sagunrajshahi1 <sagunrajshahi@gmail.com>
2026-07-09 13:18:28 -0600

    auth maintain

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+
 
 const attendanceRoute = require("./routes/attendance");
 const studentRoute=require("./routes/student");
